fe389fe73477f963942ca8031afa55c63dfee06d
Théo Sirieix <theo.sirieix@gmail.com>
2024-01-31 11:40:04 +0100

    modif routes

--- routes/travels.js
+++ routes/travels.js
@@ -3,21 +3,19 @@ var router = express.Router()
 
 const Travel = require("../models/travels") 
 
-router.get('/', (req, res) => {
-
-    Travel.find({$and:[{departure : req.body.departure}, {arrival : req.body.arrival}, {date : req.body.date}]})
-    .then(data => {
-        console.log(data);
-        if (data.length > 0) {
-		return res.json({result : true, travels : data});
-    
-       } else {
-       return res.json({result :false});
-      };
-
+router.post('/', (req, res) => {
+    let startDate = new Date(req.body.date);
+    let endDate = new Date(startDate)
+        endDate.setDate(startDate.getDate() + 1)
+    Travel.find({$and:[{departure : req.body.departure}, {arrival : req.body.arrival}, {date : {$gte: startDate, $lt: endDate}}]})
+        .then(data => {
+            if (data.length > 0) {
+                return res.json({result : true, travels : data});
+            } else {
+                return res.json({result : false, travels: data});
+            };
+        });
     });
-    
-});
 
 
 module.exports = router
